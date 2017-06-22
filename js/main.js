9105a656b9011cc5720e1c45146535318f68afd8
fhlillo <f.guicharrousse@gmail.com>
2017-06-21 22:51:35 -0400

    se crea la clase de las fichas y en js se genera el tablero

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 // JavaScript Document
+$(document).ready(function(){
+	b = 0;
+	for(i=0;i<8;i++){
+		var estructura  = "<div class='row'>";
+		for(a=0;a<8;a++){
+			estructura +="<div class='";
+			if(b == 0){
+				estructura += "blanco";
+				b = 1;
+			}else{
+				estructura +="negro";
+				b = 0;
+			}
+			estructura +="'>"
+			if(i<=2 & b == 1){
+				estructura +="<div class='ficha-rojo'></div>";
+			}else if(i>4 & b == 1){
+				estructura +="<div class='ficha-azul'></div>";				
+			}
+			estructura +="</div>";
+		}
+		if(b == 0){
+			b = 1;
+		}else{
+			b=0;
+		}
+		estructura += "</div>";
+		$("#tablero").append(estructura);
+	}
+	
+})
