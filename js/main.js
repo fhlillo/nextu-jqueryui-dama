cd743ac33ee207fced2838b2621a79dbf3e62413
fhlillo <f.guicharrousse@gmail.com>
2017-06-30 18:30:57 -0400

    se genera los moviminetos - se inicia la función para las jugadas

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	for(i=0;i<8;i++){
 		var estructura  = "<div class='row'>";
 		for(a=0;a<8;a++){
-			estructura +="<div class='";
+			estructura +="<div id='"+i+"-"+a+"' class='";
 			if(b == 0){
 				estructura += "blanco";
 				b = 1;
@@ -14,7 +14,7 @@ $(document).ready(function(){
 			}
 			estructura +="'>"
 			if(i<=2 & b == 1){
-				estructura +="<div class='ficha-rojo'></div>";
+				estructura +="<div class='ficha-rojo' id='fil-"+i+"-col-"+a+"'></div>";
 			}else if(i>4 & b == 1){
 				estructura +="<div class='ficha-azul'></div>";				
 			}
@@ -27,6 +27,40 @@ $(document).ready(function(){
 		}
 		estructura += "</div>";
 		$("#tablero").append(estructura);
+	}
+
+	$(".ficha-rojo").draggable({ revert: 'invalid', containment: "#tablero" });
+	$(".ficha-azul").draggable({ revert: 'invalid', containment: "#tablero" });
+	
+	for(i=0;i<8;i++){
+		for(a=0;a<8;a++){ 
+			var box_tablero = i+"-"+a;
+			$("#"+box_tablero).droppable({
+				drop: function(event, ui){
+					var pos_actual = $(this).attr("id").split("-");
+					var pos_anterior = ui.draggable.parent().attr("id").split("-");
+					$(ui.draggable).appendTo($(this));
+					if(ui.draggable.hasClass("ficha-rojo")){
+						if((parseInt(pos_actual[0])-2) == parseInt(pos_anterior[0])){
+							alert("comer");
+						}else{
+							alert("no comer");
+						}
+					}else{
+						alert("nulo");
+					}
+					
+					/*alert(pos_actual);
+					alert(pos_anterior);*/
+					//alert(posicion[0]);
+				}/*,
+				accept: function(d){
+					var pos_actual = $(this).attr("id").split("-");
+					var pos_anterior = ui.draggable.parent().attr("id").split("-");
+					
+				}*/
+			})
+		}
 	}
 	
 })
